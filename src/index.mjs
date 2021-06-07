8b62e14739eaa9a43e6cb10a6b30ae9efcd4dec1
Weihong Guan <weihong.guan@cpnet.io>
2021-06-07 19:12:05 +0800

    promises.allsettle

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,16 @@
-import YAML from 'yaml';
+#!/usr/bin/env node
+
 import fs from 'fs';
+import YAML from 'yaml';
+import chalk from 'chalk';
 import { spawn } from 'child_process';
 
 const verify = (cmd, { input, output }) => new Promise((resolve, reject) => {
   let actual = '';
+
+  const tim = setTimeout(() => {
+    reject({ input, output, actual: 'timeout' });
+  }, 3000);
 
   const proc = spawn(cmd);
   proc.stdin.write(input);
@@ -14,25 +21,33 @@ const verify = (cmd, { input, output }) => new Promise((resolve, reject) => {
   });
 
   proc.on('close', (code) => {
+    clearTimeout(tim);
     if (code) {
-      reject(code);
+      reject({ input, output, actual });
     } else {
       if (actual === output) {
-        resolve();
+        resolve({ input, output });
       } else {
-        reject({ actual, output });
+        reject({ input, output, actual });
       }
     }
   });
+
+
 });
 
 (async () => {
   const readme = await fs.promises.readFile(process.argv[2], 'utf-8');
   const { tests } = YAML.parse(readme);
-
-  try {
-    await Promise.all(tests.map(test => verify(process.argv[3], test)));
-  } catch (e) {
-    console.log(e);
-  }
+  const results = await Promise.allSettled(tests.map(test => verify(process.argv[3], test)));
+  // console.log(results);
+  results.forEach(({ status, reason, value }) => {
+    if (status === 'fulfilled') {
+      const { input, output } = value;
+      console.log(`input: ${input}\t output: ${output} \t ${chalk.green('SUCCESS')}`);
+    } else {
+      const { input, output, actual } = reason;
+      console.log(`input: ${input}\t output: ${output} \t ${chalk.red('FAILED')}`);
+    }
+  });
 })();
